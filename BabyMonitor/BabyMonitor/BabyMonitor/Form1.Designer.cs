1dfde58d5585c82f87f21c807af297a4bb8b6d87
GabrielDinca25 <gdincagabriel@gmail.com>
2019-04-06 19:12:20 +0300

    Added app

---
+++ BabyMonitor/BabyMonitor/BabyMonitor/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace BabyMonitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.connect = new System.Windows.Forms.Button();
+            this.start = new System.Windows.Forms.Button();
+            this.stop = new System.Windows.Forms.Button();
+            this.openLog = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.write = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.SuspendLayout();
+            // 
+            // connect
+            // 
+            this.connect.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.connect.Font = new System.Drawing.Font("Bahnschrift Condensed", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.connect.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.connect.Location = new System.Drawing.Point(284, 83);
+            this.connect.Name = "connect";
+            this.connect.Size = new System.Drawing.Size(197, 61);
+            this.connect.TabIndex = 0;
+            this.connect.Text = "Connect to Baby Monitor";
+            this.connect.UseVisualStyleBackColor = false;
+            this.connect.Click += new System.EventHandler(this.connect_Click);
+            // 
+            // start
+            // 
+            this.start.BackColor = System.Drawing.Color.Green;
+            this.start.Font = new System.Drawing.Font("Bahnschrift SemiBold", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.start.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.start.Location = new System.Drawing.Point(284, 181);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(93, 36);
+            this.start.TabIndex = 1;
+            this.start.Text = "Start ";
+            this.start.UseVisualStyleBackColor = false;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // stop
+            // 
+            this.stop.BackColor = System.Drawing.Color.Crimson;
+            this.stop.Font = new System.Drawing.Font("Bahnschrift SemiBold", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stop.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.stop.Location = new System.Drawing.Point(388, 181);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(93, 36);
+            this.stop.TabIndex = 2;
+            this.stop.Text = "Stop ";
+            this.stop.UseVisualStyleBackColor = false;
+            this.stop.Click += new System.EventHandler(this.stop_Click_1);
+            // 
+            // openLog
+            // 
+            this.openLog.Location = new System.Drawing.Point(284, 262);
+            this.openLog.Name = "openLog";
+            this.openLog.Size = new System.Drawing.Size(197, 46);
+            this.openLog.TabIndex = 3;
+            this.openLog.Text = "Open log file";
+            this.openLog.UseVisualStyleBackColor = true;
+            this.openLog.Click += new System.EventHandler(this.openLog_Click);
+            // 
+            // label2
+            // 
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(284, 147);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(197, 23);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Not connected";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // write
+            // 
+            this.write.Location = new System.Drawing.Point(284, 223);
+            this.write.Name = "write";
+            this.write.Size = new System.Drawing.Size(197, 23);
+            this.write.TabIndex = 7;
+            this.write.Text = "Write to file";
+            this.write.UseVisualStyleBackColor = true;
+            this.write.Click += new System.EventHandler(this.write_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.write);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.openLog);
+            this.Controls.Add(this.stop);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.connect);
+            this.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button connect;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Button stop;
+        private System.Windows.Forms.Button openLog;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button write;
+        private System.IO.Ports.SerialPort serialPort1;
+    }
+}
+
---
+++ BabyMonitor/BabyMonitor/BabyMonitor/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Diagnostics;
+using System.IO;
+using System.IO.Ports;
+
+namespace BabyMonitor
+{
+    public partial class Form1 : Form
+    {
+        string m_logFilePath;
+        int count = 0;
+        public Form1()
+        {
+            System.Diagnostics.Debug.WriteLine("Here");
+
+            m_logFilePath = "";
+            InitializeComponent();
+
+            serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(DataReceived);
+
+        }
+
+        private void DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                if(m_logFilePath != "")
+                {
+                    SerialPort spl = (SerialPort)sender;
+                    var dataRcv = spl.ReadLine();
+
+                    switch(dataRcv)
+                    {
+                        case "1":
+                            string dt1 = DateTime.Now.ToString();
+                            System.Diagnostics.Debug.WriteLine("Motion detected at: " + dt1);
+                            System.IO.File.AppendAllText(@m_logFilePath, "Motion detected at: " + dt1 + "\n");
+                            break;
+                        case "0":
+                            string dt2 = DateTime.Now.ToString();
+                            System.Diagnostics.Debug.WriteLine("Motion ended at: " + dt2);
+                            System.IO.File.AppendAllText(@m_logFilePath, "Motion ended at: " + dt2 + "\n");
+                            break;
+                        default:
+                            break;
+                    }
+
+                    System.Diagnostics.Debug.WriteLine("Data received" + dataRcv);
+
+                }
+
+                count++;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void connect_Click(object sender, EventArgs e)
+        {
+            serialPort1.PortName = "COM5";
+            serialPort1.BaudRate = 9600;
+
+            try
+            {
+                System.Diagnostics.Debug.WriteLine("Trying to open serial port");
+
+                if (!serialPort1.IsOpen)
+                {
+                    serialPort1.Open();
+                    System.Diagnostics.Debug.WriteLine("Serial Port Open");
+
+                    label2.Text = "Connected to Baby Monitor";
+                    label2.ForeColor = Color.Green;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            Stream myStream;
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+            saveFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.RestoreDirectory = true;
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if ((myStream = saveFileDialog1.OpenFile()) != null)
+                {
+                    // Code to write the stream goes here.
+                    myStream.Close();
+                    m_logFilePath = saveFileDialog1.FileName;
+                }
+            }
+        }
+
+        private void openLog_Click(object sender, EventArgs e)
+        {
+            if (m_logFilePath != "")
+            {
+                Process.Start(m_logFilePath);
+            }
+        }
+
+        private void write_Click(object sender, EventArgs e)
+        {
+            if(m_logFilePath != "")
+            {
+                string dt = DateTime.Now.ToString();
+
+                System.IO.File.AppendAllText(@m_logFilePath, dt);
+                System.IO.File.AppendAllText(@m_logFilePath, "\n");
+            }
+
+        }
+
+        private void stop_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                if (serialPort1.IsOpen)
+                {
+                    serialPort1.Close();
+
+                    m_logFilePath = "";
+
+                    label2.Text = "Not connected";
+                    label2.ForeColor = Color.Red;
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+        }
+    }
+}
